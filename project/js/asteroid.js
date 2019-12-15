7ed52593ea6875b8d6c492a1ba8dd0319ba72af5
Delvis Apud <deapud@gmail.com>
2019-12-15 10:28:46 -0500

    adding asteroids class

--- project/js/asteroid.js
+++ project/js/asteroid.js
@@ -1,3 +1,43 @@
 class Asteroid {
+    constructor(x, y, radius, level, collisionRadius) {
+        this.visible = true;
+        this.x = x || Math.floor(Math.random() * canvasWidth - 20);
+        this.y = y || Math.floor(Math.random() * canvasHeight - 20);
+        this.speed = 3;
+        this.radius = radius || 50;
+        this.angle = Math.floor(Math.random() * 359);
+        this.strokeColor = 'white';
+        this.collisionRadius = collisionRadius || 46;
+        // Used to decide if this asteroid can be broken into smaller pieces
+        this.level = level || 1; //the biggest---3levels
+
+    }
+    update() {
+        let radians = this.angle / Math.PI * 180;
+        this.x += Math.cos(radians) * this.speed;
+        this.y += Math.sin(radians) * this.speed;
+        if (this.x < this.radius) {
+            this.x = canvas.width;
+        }
+        if (this.x > canvas.width) {
+            this.x = this.radius;
+        }
+        if (this.y < this.radius) {
+            this.y = canvas.height;
+        }
+        if (this.y > canvas.height) {
+            this.y = this.radius;
+        }
+    }
+    draw(ctx) {
+        ctx.beginPath();
+        let vertAngle = ((Math.PI * 2) / 6);
+        var radians = this.angle / Math.PI * 180;
+        for (let i = 0; i < 6; i++) {
+            ctx.lineTo(this.x - this.radius * Math.cos(vertAngle * i + radians), this.y - this.radius * Math.sin(vertAngle * i + radians));
+        }
+        ctx.closePath();
+        ctx.stroke();
+    }
 
 }
